247ef5c47b69b1085a548c7153d4ace726dd1f14
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-11 18:42:12 -0300

    feat: add hour selection functionality with click events

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { openingHours } from '../../utils/opening-hours.js';
+import { hoursClick } from '../form/hours-click.js';
 
 const hours = document.getElementById('hours');
 
@@ -26,6 +27,27 @@ export function hoursLoad({ date }) {
     li.classList.add(available ? 'hour-available' : 'hour-unavailable');
 
     li.textContent = hour;
+
+    if (hour === '9:00') {
+      hourHeaderAdd('Manhã');
+    } else if (hour === '13:00') {
+      hourHeaderAdd('Tarde');
+    } else if (hour === '18:00') {
+      hourHeaderAdd('Noite');
+    }
+
     hours.append(li);
   });
+
+  // Adiciona o evento de clique nos horários disponíveis.
+  hoursClick();
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement('li');
+
+  header.classList.add('hour-period');
+  header.textContent = title;
+
+  hours.append(header);
 }
